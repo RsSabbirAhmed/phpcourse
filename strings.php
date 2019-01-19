56d9e0cc6a29234d572c34703ea4e0d397d1d299
Rs Sabbir Ahmed <rssabbirahmed1996@gmail.com>
2019-01-19 23:27:12 +0600

    v 2

---
+++ strings.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+  <?php
+  /*   echo "my name is Sabbir Ahmed <br>";
+    echo 'my age is 21 <br>';
+
+    $color = 'red';
+    $shirt = "sleev-less";
+    $combination = $color. " " .$shirt;
+    echo $combination; */
+   ?><br>
+  <?php
+  $color = 'red';
+   echo "${color}is my fav <br>";
+    echo '$color <br>'; ?>
+
+  <?php  ?>
+
+  <?php?>
+</body>
+</html>
